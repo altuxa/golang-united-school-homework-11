c84a6f5a7133a25d841eebb068f111eb06c3e91b
Altynbek <altuxadark98@gmail.com>
2022-07-03 18:27:52 +0600

    slow need fix

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,8 @@
 package batch
 
 import (
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -14,5 +16,25 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, 0, n)
+	buf := make(chan struct{}, pool)
+	var wg sync.WaitGroup
+	var ops int64
+	var m sync.Mutex
+	for i := 0; i < int(n); i++ {
+		wg.Add(1)
+		buf <- struct{}{}
+		go func() {
+			m.Lock()
+			user := getOne(int64(ops))
+			atomic.AddInt64(&ops, 1)
+			res = append(res, user)
+			m.Unlock()
+			<-buf
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	close(buf)
+	return res
 }
